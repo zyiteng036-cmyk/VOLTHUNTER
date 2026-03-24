b966cd225d2ee155cf95bcc8431e6cc41ec4f60f
zyiteng036-cmyk <zyiteng036@gmail.com>
2026-03-25 04:05:30 +0900

    Delete 担当箇所（ソース） directory

--- 担当箇所（ソース）/CollisionChannelName.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Engine/EngineTypes.h"
-
-//ジャスト回避用コリジョンチャンネル
-constexpr ECollisionChannel JustEvasive_Collision = ECC_GameTraceChannel1;
-
-//攻撃判定用コリジョンチャンネル
-constexpr ECollisionChannel AttackDetection_Collision = ECC_GameTraceChannel2;
